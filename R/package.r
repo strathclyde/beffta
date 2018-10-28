23fbb04ba35c560c870f1f8227ae6f4c28754e52
bautheac <o.bautheac@gmail.com>
2018-10-28 18:10:21 +0000

    Start customizing site

--- R/package.r
+++ R/package.r
@@ -1,6 +1,6 @@
 #' befftafall2018: becoming an effective technology analyst.
 #'
-#' This package hosts assignment material for the "becoming an effective technology analyst"
+#' This package hosts assignment material for the 'becoming an effective technology analyst'
 #'   fall 2018 class as part of the Strathclyde Business School MSc fintech program coursework.
 #'
 #' @section assignment:
